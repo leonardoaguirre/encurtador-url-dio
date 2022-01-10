11dea7ff3f31bc99db6c939d82f317cc9112e8ee
leonardoaguirre <62442754+leonardoaguirre@users.noreply.github.com>
2022-01-10 19:12:33 -0300

    Adiciona consulta e insert com o mogoModel na controller

--- src/controller/URLController.ts
+++ src/controller/URLController.ts
@@ -1,24 +1,31 @@
-import { config } from '../config/constants';
+import { configs } from '../config/constants';
 import { Request, Response } from "express";
 import shortId from 'shortid';
+import { UrlModel } from '../model/Url';
 
 export class URLController {
     async encurtar(request: Request, response: Response): Promise<void> {
         const { originUrl } = request.body
+        const url = await UrlModel.findOne({ originUrl })
+
+        if (url) {
+            response.json(url)
+            return
+        }
         const hash = shortId.generate()
+        const shortUrl = `${configs.API_URL}/${hash}`
 
-        const shortUrl = `${config.API_URL}/${hash}`
+        const novaUrl = await UrlModel.create({ hash, shortUrl, originUrl })
 
-        response.json({ originUrl, hash, shortUrl })
+        response.json(novaUrl)
     }
     async redirecionar(request: Request, response: Response): Promise<void> {
-        const {hash} = request.params
-        const url = { 
-            originUrl : 'https://www.youtube.com/',
-            hash : 'jSJlb6lOG',
-            shortUrl : 'http://localhost:5000/jSJlb6lOG'
-        }
+        const { hash } = request.params
 
-        response.redirect(url.originUrl)
+        const url = await UrlModel.findOne({ hash })
+        if (url) {
+            response.redirect(url.originUrl)
+        }
+        response.status(400).json('Esta URL nao existe')
     }
 }
